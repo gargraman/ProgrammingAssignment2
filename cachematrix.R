8c641c4de5b20ee5d0267beffb22d20ce62b9122
ramgarg <raman.g.garg@oracle.com>
2015-07-25 00:35:11 +0530

    code for Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function(y){
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function(){
+    x
+  }
+  setinverse <- function(invers){
+    mat <<- invers
+  }
+  getinverse <- function() mat
+  list(set = set, get=get,setinverse=setinverse,getinverse=getinverse)
+  
 }
 
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat <- x$getinverse()
+  if(!is.null(mat)){
+    message("getting cached inverse")
+    return (mat)
+  }
+  data <- x$get()
+  mat <- solve(data,...)
+  x$setinverse(mat)
+  mat
 }
