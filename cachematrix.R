1f73f21b598eb2e8055a461ce0d31abc7ac89294
ramgarg <raman.g.garg@oracle.com>
2015-07-25 00:43:59 +0530

    Changes for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
+#########################
+##
+##get()
+##set(y)
+##getinverse()
+##setinverse(invers)
+##
+#########################
 
 makeCacheMatrix <- function(x = matrix()) {
   mat <- NULL
@@ -22,7 +27,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##########################################################
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   mat <- x$getinverse()
